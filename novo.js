94fe4a10cfadf4412849ef01ca552465fc5cf08b
Fabio <fabiodeoliveira07@gmail.com>
2018-10-19 08:23:24 -0300

    Adicionando arquivos do projeto

---
+++ novo.js
@@ -0,0 +1,97 @@
+if (window.self === window.top) {
+  document.domain = 'deezer.com';
+}
+var DEEZER_DRIVE_DOWNLOAD = 'http://dzr.fm/download';
+var WIN8_APP_DOWNLOAD = 'http://apps.microsoft.com/windows/app/deezer/e48b595e-b20a-4eb3-968c-c20b2cadaa1e?';
+var DIRECTION = "ltr";
+var VOICECALLBACK = "";
+var TOTAL_TRACKS = '53';
+var PLAYER_INIT = {
+  type: 'track_page',
+  track: {
+    "data": [{
+      "SNG_ID": "73045137",
+      "PRODUCT_TRACK_ID": "199611381",
+      "UPLOAD_ID": 0,
+      "SNG_TITLE": "Rainfall",
+      "ART_ID": "429267",
+      "PROVIDER_ID": "50",
+      "ART_NAME": "Foreign Cinema",
+      "ARTISTS": [{
+        "ART_ID": "429267",
+        "ROLE_ID": "0",
+        "ARTISTS_SONGS_ORDER": "0",
+        "ART_NAME": "Foreign Cinema",
+        "ART_PICTURE": "",
+        "SMARTRADIO": 0,
+        "RANK": "38101",
+        "LOCALES": [],
+        "__TYPE__": "artist"
+      }],
+      "ALB_ID": "7187289",
+      "ALB_TITLE": "Monochromatic",
+      "TYPE": 0,
+      "MD5_ORIGIN": "b4d4b72a410af1710254affaef7c2f99",
+      "VIDEO": false,
+      "DURATION": "324",
+      "ALB_PICTURE": "846f6a56e61bc2497b5f8cd541227917",
+      "ART_PICTURE": "",
+      "RANK_SNG": "0",
+      "SMARTRADIO": 0,
+      "FILESIZE_AAC_64": 0,
+      "FILESIZE_MP3_64": "0",
+      "FILESIZE_MP3_128": "5179383",
+      "FILESIZE_MP3_256": 0,
+      "FILESIZE_MP3_320": "12922297",
+      "FILESIZE_FLAC": "32158949",
+      "FILESIZE": "5179383",
+      "MEDIA_VERSION": "0",
+      "DISK_NUMBER": "1",
+      "TRACK_NUMBER": "1",
+      "VERSION": "",
+      "MEDIA": [{
+        "TYPE": "preview",
+        "HREF": "https:\/\/cdns-preview-b.dzcdn.net\/stream\/c-b15243e65d965fd9ef0060b8505c226c-0.mp3"
+      }],
+      "EXPLICIT_LYRICS": "0",
+      "RIGHTS": {
+        "STREAM_ADS_AVAILABLE": true,
+        "STREAM_ADS": "2000-01-01",
+        "STREAM_SUB_AVAILABLE": true,
+        "STREAM_SUB": "2000-01-01"
+      },
+      "ISRC": "USHM21207727",
+      "HIERARCHICAL_TITLE": "",
+      "SNG_CONTRIBUTORS": [],
+      "LYRICS_ID": 0,
+      "COPYRIGHT": "2012 Vibraphone",
+      "PHYSICAL_RELEASE_DATE": "0000-00-00",
+      "S_MOD": 1,
+      "S_PREMIUM": 1,
+      "DATE_START_PREMIUM": "2000-01-01",
+      "DATE_START": "2000-01-01",
+      "STATUS": 1,
+      "USER_ID": 0,
+      "URL_REWRITING": "foreign-cinema",
+      "SNG_STATUS": "1",
+      "AVAILABLE_COUNTRIES": {
+        "STREAM_ADS": ["AE", "AF", "AG", "AI", "AL", "AM", "AN", "AO", "AQ", "AR", "AS", "AT", "AU", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BN", "BO", "BR", "BT", "BV", "BW", "BY", "CA", "CC", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CX", "CY", "CZ", "DE", "DJ", "DK", "DM", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FM", "FR", "GA", "GB", "GD", "GE", "GH", "GM", "GN", "GQ", "GR", "GS", "GT", "GU", "GW", "HK", "HM", "HN", "HR", "HU", "ID", "IE", "IL", "IN", "IO", "IQ", "IR", "IS", "IT", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MD", "ME", "MG", "MH", "MK", "ML", "MM", "MN", "MP", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NE", "NF", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PG", "PH", "PK", "PL", "PN", "PS", "PT", "PW", "PY", "QA", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SI", "SJ", "SK", "SL", "SN", "SO", "ST", "SV", "SY", "SZ", "TC", "TD", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TV", "TW", "TZ", "UA", "UG", "UM", "US", "UY", "UZ", "VC", "VE", "VG", "VI", "VN", "VU", "WS", "YE", "ZA", "ZM", "ZW"],
+        "STREAM_SUB_ONLY": []
+      },
+      "__TYPE__": "song",
+      "DIGITAL_RELEASE_DATE": "0000-00-00"
+    }],
+    "count": 1,
+    "total": 1
+  },
+  index: 0,
+  auto_play: false,
+  show_lyrics: false,
+  element_id: 73045137
+};
+var SPONSORED_TRACK_CONFIG = {
+  "token": "8840cfb7-e05c-4e3e-90d2-4413bb9661fe",
+  "progress_call_frequency": 15,
+  "api_key": "d4a7196babfd6a0a324aece0d4680ef0",
+  "api_url": "https:\/\/api.ffmapi.com\/"
+};
